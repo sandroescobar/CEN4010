5785a3e506b4c2963b1e131aa916b7248edfddb3
Alejandro Alonso <116370266+AlexCs3008@users.noreply.github.com>
2024-11-19 13:53:46 -0500

    Update homePageLoggedIn.swift

--- homePageLoggedIn.swift
+++ homePageLoggedIn.swift
@@ -3,37 +3,39 @@
 //  MiamiMarketStore
 //
 //  Created by Alessandro Escobar on 11/8/24.
-//
+// Edited by Alejandro Alonso on 11/19/24
 
 import SwiftUI
 
 struct homePageLoggedIn: View {
     @State private var tableSelection = 1
+    
     var body: some View {
-        TabView(selection: $tableSelection){
+        TabView(selection: $tableSelection) {
             LocationPage()
-                .tabItem{
-                    
-                        
-            }
+                .tabItem {
+                    Image(systemName: "map.fill")
+                    Text("Locations")
+                }
+                .tag(1)
+            
             PreferencePage()
-                .tabItem{
-                        
-            }
+                .tabItem {
+                    Image(systemName: "list.bullet.clipboard.fill")
+                    Text("Preferences")
+                }
+                .tag(2)
+            
             SettingPage()
-                .tabItem{
-                        
-            }
-    }
-        .overlay(alignment: .bottom){
-            //custonTabView
-            CustomTabView(tableSelection: $tableSelection)
-            
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+                .tag(3)
         }
-            
-            
-        
-        
+        .overlay(alignment: .bottom) {
+            CustomTabView(tableSelection: $tableSelection)
+        }
     }
 }
 
